6e1865959c51a6e9f68f9693bdb685ac2a39371e
Adrián Estrada <edsadr@gmail.com>
2018-05-02 09:50:15 -0500

    Patching proxy for N|Solid v3 (#1)
    * Patching agents detection for N|Solid v3 and adding v3 new events
    * vulnerabilities and event block adapted to v2 and v3 calls

--- buildMessage.js
+++ buildMessage.js
@@ -1,12 +1,15 @@
+
 const messages = {
-  'nsolid-agent-online': agentOnline,
-  'nsolid-agent-offline': agentOffline,
-  'nsolid-cpu-threshold-exceeded': cpuThresholdExceeded,
-  'nsolid-heap-threshold-exceeded': heapThresholdExceeded,
-  'nsolid-vulnerability-detected': vulnerabilityDetected,
-  'nsolid-snapshot-generated': snapshot,
-  'nsolid-profile-generated': profile,
-  'nsolid-process-blocked': processBlocked
+  'nsolid-saved-view-match': savedView, // v3
+  'nsolid-agent-exit': agentExit, // v3
+  'nsolid-agent-online': agentOnline, // v2
+  'nsolid-agent-offline': agentOffline, // v2
+  'nsolid-cpu-threshold-exceeded': cpuThresholdExceeded, // v2
+  'nsolid-heap-threshold-exceeded': heapThresholdExceeded, // v2
+  'nsolid-vulnerability-detected': vulnerabilityDetected, //v2 and v3
+  'nsolid-snapshot-generated': snapshot, // v2 and v3
+  'nsolid-profile-generated': profile, // v2 and v3
+  'nsolid-process-blocked': processBlocked // v2 and v3
 }
 
 module.exports = function buildMessage (task) {
@@ -50,7 +53,18 @@ function heapThresholdExceeded (evt) {
 }
 
 function vulnerabilityDetected (evt) {
-  return `> Vulnerability found in ${evt.vulnerability.package}@${evt.vulnerability.vulnerable} in ${describeAgent(evt)}.`
+  if (evt.vulnerability) {
+    return `> Vulnerability found in ${evt.vulnerability.package}@${evt.vulnerability.vulnerable} in ${describeAgent(evt)}.`
+  } else if (evt.vulnerabilities) {
+    let vuln = 'Vulnerability found in \n'
+    for (let i = 0, length1 = evt.vulnerabilities.length; i < length1; i++) {
+      vuln += `${evt.vulnerabilities[i].package}@${evt.vulnerabilities[i].vulnerable} in ${describeAgent(evt)}. \n`
+    }
+    return vuln
+  } else {
+    return 'Unknown vulnerability package on Unknown version'
+  }
+
 }
 
 function snapshot (evt) {
@@ -62,19 +76,40 @@ function profile (evt) {
 }
 
 function processBlocked (evt) {
-  const trace = evt.stacktrace.replace('```', `'''`)
+  const trace = (evt.stacktrace) ? evt.stacktrace.replace('```', `'''`) : evt.stack.replace('```', `'''`)
   const lines = trace.split('\n')
   const first = lines.shift()
   const result = '> ' + first + ' ```' + lines.join('\n') + '```'
   return result
 }
 
+function savedView (evt) {
+  return `> Saved view ${evt.config.name} match: ${describeAgent(evt)}`
+}
+
+function agentExit (evt) {
+  return `> Agent exit: ${describeAgent(evt)}`
+}
+
 function describeAgent (evt) {
-  const agent = evt.agent || {}
-  const pid = agent.pid || 'Unknown pid'
-  const hostname = agent.hostname || 'Unknown hostname'
-  const app = evt.app || 'Unknown application'
-  return `${app} (${pid}) on ${hostname}`
+  if (evt.agent) {
+    const agent = evt.agent || {}
+    const pid = agent.pid || 'Unknown pid'
+    const hostname = agent.hostname || 'Unknown hostname'
+    const app = evt.app || 'Unknown application'
+
+    return `${app} (${pid}) on ${hostname}`
+  } else if (evt.agents) {
+    let agents = '\n'
+    for (let i = 0, length1 = evt.agents.length; i < length1; i++) {
+      let { app, pid, hostname } = evt.agents[i].info
+      agents += `${app} (${pid}) on ${hostname} \n`
+    }
+
+    return agents
+  } else {
+    return 'Unknown application on Unknown hostname'
+  }
 }
 
 function toPercent (num) {
